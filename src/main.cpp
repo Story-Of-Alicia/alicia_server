21f8dbfcd68b218db5fc014483e2111efa2901b4
rgnter <maros.prejsa1@gmail.com>
2024-05-20 13:38:57 +0200

    Add AcCmdCLLogin

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <array>
 #include <cassert>
+#include <fstream>
 #include <unordered_map>
 
+#include <istream>
+#include <ostream>
 #include <boost/asio.hpp>
 
 namespace alicia {
+
+  //! Appy XORcodec to buffer.
+  //!
+  //! @param buffer Buffer.
+  //! @returns XORcoded buffer.
+  template <typename Buffer> void xor_codec_cpp(Buffer& buffer)
+  {
+    const std::array control{
+        static_cast<std::byte>(0xCB),
+        static_cast<std::byte>(0x91),
+        static_cast<std::byte>(0x01),
+        static_cast<std::byte>(0xA2),
+    };
+
+    for(size_t idx = 0; idx < buffer.size(); idx++) {
+      const auto shift = idx % 4;
+      buffer[idx] ^= control[shift];
+    }
+  }
 
   /**
    * Encode message information.
@@ -60,61 +82,135 @@ void test_magic()
 }
 
 namespace asio = boost::asio;
-namespace alicia::protocol
-{
+
+namespace alicia {
+
+  template <typename ValType> void read(std::istream& stream, ValType& val)
+  {
+    stream.read(reinterpret_cast<char*>(&val), sizeof(val));
+  }
+
+  void read(std::istream& stream, std::string& val)
+  {
+    while(true) {
+      char v{0};
+      stream.read(&v, sizeof(v));
+      if(v == 0)
+        break;
+      val += v;
+    }
+  }
+
+  template <typename ValType> void read(std::istream& stream, std::vector<ValType>& val)
+  {
+    for(auto& v : val) {
+      read(stream, v);
+    }
+  }
+
+  std::array message_table = {0, 1};
+
+  class AcCmdCLLogin {
+  public:
+    uint16_t constant0;
+    uint16_t constant1;
+    std::string login_id;
+    uint32_t member_no;
+    std::string auth_key;
+
+    void Serialize(std::istream& buffer)
+    {
+      read(buffer, constant0);
+      read(buffer, constant1);
+      read(buffer, login_id);
+      read(buffer, member_no);
+      read(buffer, auth_key);
+    }
+  };
+
+  struct AcCmdCLLoginOK {};
+
+  struct AcCmdCLLoginCancel {};
+
   /**
    * Client
    */
-  class Client
-  {
+  class Client {
   public:
-    explicit Client(asio::ip::tcp::socket&& socket) noexcept : _socket(std::move(socket))
-    {}
+    explicit Client(asio::ip::tcp::socket&& socket) noexcept : _socket(std::move(socket)) {}
+
   public:
     void read_loop()
     {
-      _socket.async_read_some(
-        asio::mutable_buffer(_buffer.data(), _buffer.size()),
-        [&](boost::system::error_code error, std::size_t size)
-          {
-            if (error)
-            {
-              printf(
-                "Error occurred on read loop with client on port %d. What: %s\n",
-                 _socket.remote_endpoint().port(),
-                 error.message().c_str());
-              return;
-            }
-            printf("Read some data (%lld bytes) from client on port %d.\n", size, _socket.remote_endpoint().port());
-            printf("\t");
-            for (int i = 0; i < size; ++i) {
-              printf("%X ", _buffer[i]);
-            }
-            printf("\n");
+      _socket.async_read_some(_buffer.prepare(4096), [&](boost::system::error_code error, std::size_t size) {
+        if(error) {
+          printf(
+              "Error occurred on read loop with client on port %d. What: %s\n",
+              _socket.remote_endpoint().port(),
+              error.message().c_str());
+          return;
+        }
 
-            // Respond with AcCmdCLLoginCancel with response code
-            // CR_INVALID_VERSION
-            std::array<std::byte, 5> test;
-            *reinterpret_cast<uint32_t*>(test.data()) = encode_message_information(9, 16384, 5);
-            test[4] = static_cast<std::byte>(3);
-            _socket.write_some(boost::asio::const_buffer(test.data(), 5));
+        // Commit the recieved buffer.
+        _buffer.commit(size);
+        std::istream stream(&_buffer);
 
-            read_loop();
-          });
+        // Read the message magic.
+        uint32_t magic = 0;
+        read(stream, magic);
+        if(magic == 0x0) {
+          throw std::runtime_error("invalid message magic");
+        }
+
+        const auto message_length = decode_message_length(magic);
+
+        // Read the message data.
+        std::vector<std::byte> data;
+        data.resize(message_length);
+        read(stream, data);
+
+        // XOR decode
+        xor_codec_cpp(data);
+
+        asio::streambuf data_buf;
+        data_buf.prepare(data.size());
+        std::ostream data_buf_o(&data_buf);
+
+        for(auto value : data) {
+          data_buf_o << bit_cast<uint8_t>(value);
+        }
+
+        std::istream data_stream(&data_buf);
+
+        AcCmdCLLogin login;
+        login.Serialize(data_stream);
+        printf("AcCmdCLLogin: \n");
+        printf("\tconstant0: %d\n", login.constant0);
+        printf("\tconstant1: %d\n", login.constant1);
+        printf("\tLogin ID: %s\n", login.login_id.c_str());
+        printf("\tMember NO.: %d\n", login.member_no);
+        printf("\tAuth Key: %s\n", login.auth_key.c_str());
+
+        std::array<std::byte, 5> test;
+        *reinterpret_cast<uint32_t*>(test.data()) = encode_message_information(9, 16384, 5);
+        test[4] = static_cast<std::byte>(3);
+        _socket.write_some(boost::asio::const_buffer(test.data(), 5));
+
+        read_loop();
+      });
     }
+
   private:
     asio::ip::tcp::socket _socket;
-    std::array<std::byte, 4092> _buffer;
+    asio::streambuf _buffer;
   };
 
   /**
    * Server
    */
-  class Server
-  {
+  class Server {
   public:
-    Server() : _acceptor(_io_ctx)
-    {}
+    Server() : _acceptor(_io_ctx) {}
 
   public:
     void host()
@@ -127,25 +223,18 @@ namespace alicia::protocol
       accept_loop();
     }
 
-    void run()
-    {
-      _io_ctx.run();
-    }
+    void run() { _io_ctx.run(); }
 
   private:
     void accept_loop()
     {
-      _acceptor.async_accept(
-        [&](boost::system::error_code error, asio::ip::tcp::socket client_socket)
-         {
-           printf("Accepted new client from port %d\n", client_socket.remote_endpoint().port());
-           const auto [itr, _] = _clients.emplace(
-             client_id++,
-             std::move(client_socket));
-           itr->second.read_loop();
+      _acceptor.async_accept([&](boost::system::error_code error, asio::ip::tcp::socket client_socket) {
+        printf("Accepted new client from port %d\n", client_socket.remote_endpoint().port());
+        const auto [itr, _] = _clients.emplace(client_id++, std::move(client_socket));
+        itr->second.read_loop();
 
-           accept_loop();
-         });
+        accept_loop();
+      });
     }
 
   private:
@@ -155,11 +244,12 @@ namespace alicia::protocol
     uint32_t client_id = 0;
     std::unordered_map<uint32_t, Client> _clients;
   };
-}
+
+} // namespace alicia
 
 int main()
 {
-  alicia::protocol::Server server;
+  alicia::Server server;
   server.host();
   server.run();
   return 0;
